b5ae7c418f57e1ab60b681239e59aaee9139acbb
Alexander Lochmann <alexander.lochmann@tu-dortmund.de>
2022-04-06 11:40:00 +0200

    Only start tmux automatically when a interactive shell is detected

--- tmux/start-tmux.sh
+++ tmux/start-tmux.sh
@@ -34,35 +34,51 @@ if [ "${_tty#/dev/ttyS}" != "$_tty" ] && [ "${_tty#/dev/ttyAMA}" != "$_tty" ]; t
 	echo
 elif [ -z ${TMUX} ];
 then
-	case "$TERM" in
-		*screen*)
-			# Handle nesting
-			if [ -n "$SSH_CONNECTION" ] && [ "$(printf "$SSH_CONNECTION" | awk '{print $1}')" != "$(printf "$SSH_CONNECTION" | awk '{print $3}')" ]; then
-				# Safeguard against ssh-ing into ourself, which causes an infinite loop
-				$HOME/.tmux/tmux-select-session.py
-			else
-				echo "INFO: Disabling auto-launch of tmux in this SSH connection, to avoid a potential infinite loop" 1>&2
-				echo "INFO: You can still run 'tmux' manually at the command line, if you know what you're doing" 1>&2
+	case "$-" in
+		*i*)
+			# Attempt to merge shell history across sessions/windows (works with some exceptions)
+			for i in shopt setopt;
+			do
+				if eval $BYOBU_TEST $i >/dev/null; then
+					case $i in
+						shopt) $i -s histappend || true ;;
+						setopt) $i appendhistory || true ;;
+					esac
+				fi
+			done
+			[ -n "$PROMPT_COMMAND" ] && PROMPT_COMMAND="history -a;history -r;$PROMPT_COMMAND" || PROMPT_COMMAND="history -a;history -r"
+			case "$TERM" in
+				*screen*)
+					# Handle nesting
+					if [ -n "$SSH_CONNECTION" ] && [ "$(printf "$SSH_CONNECTION" | awk '{print $1}')" != "$(printf "$SSH_CONNECTION" | awk '{print $3}')" ]; then
+						# Safeguard against ssh-ing into ourself, which causes an infinite loop
+						$HOME/.tmux/tmux-select-session.py
+					else
+						echo "INFO: Disabling auto-launch of tmux in this SSH connection, to avoid a potential infinite loop" 1>&2
+						echo "INFO: You can still run 'tmux' manually at the command line, if you know what you're doing" 1>&2
+						true
+					fi
+				;;
+				dumb)
+					# Dumb terminal, don't launch
+					false
+				;;
+				*)
+					$HOME/.tmux/tmux-select-session.py
+				;;
+			esac
+			# Wait very briefly for the no-logout flag to get written?
+			sleep 0.1
+			if [ -e "$HOME/.tmux/no-logout" ]; then
+				# The user does not want to logout on byobu detach
+				rm -f "$HOME/.tmux/no-logout"	# Remove one-time no-logout flag, if it exists
 				true
+			else
+				exit 0
 			fi
 		;;
-		dumb)
-			# Dumb terminal, don't launch
-			false
-		;;
-		*)
-			$HOME/.tmux/tmux-select-session.py
-		;;
 	esac
-	# Wait very briefly for the no-logout flag to get written?
-	sleep 0.1
-	if [ -e "$HOME/.tmux/no-logout" ]; then
-		# The user does not want to logout on byobu detach
-		rm -f "$HOME/.tmux/no-logout"	# Remove one-time no-logout flag, if it exists
-		true
-	else
-		exit 0
-	fi
+
 fi
 unset _tty
 true
