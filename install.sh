8b91ab77b0fe7a0ee00e25aad892e085f69dfed3
Alexander Lochmann <info@alexander-lochmann.de>
2023-01-06 23:05:32 +0100

    install.sh: Be path-independent

--- install.sh
+++ install.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
+DIR=`dirname ${0}`
+
 if [ -e "$HOME/.tmux.conf" ]; then
 	printf "Found existing .tmux.conf in your \$HOME directory. Will create a backup at $HOME/.tmux.conf.bak\n"
 fi
 cp -f "$HOME/.tmux.conf" "$HOME/.tmux.conf.bak" 2>/dev/null || true
 
-cp -a ./tmux/. $HOME/.tmux/
+cp -a ${DIR}/tmux/. $HOME/.tmux/
 ln -sf .tmux/tmux.conf $HOME/.tmux.conf
 
 echo ". \$HOME/.tmux/start-tmux.sh" >> $HOME/.profile
